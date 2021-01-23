86775c72285576594d41d97c8675809f8b553cd2
TaylanALTUN <taylanaltun@gmail.com>
2021-01-23 18:49:10 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public  Customer AddCustomer(int Id, string name, string surname, string job, double monthlyIncome)
+        {
+            Customer customer = new Customer();
+            customer.Id = Id;
+            customer.Name = name;
+            customer.Surname = surname;
+            customer.Job = job;
+            customer.MonthlyIncome = monthlyIncome;
+
+            return customer;
+        }
+
+        public  void ListCustomers(List<Customer> customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Adı " + customer.Name
+                    + ", Soyadı : " + customer.Surname
+                    + ", Mesleği : " + customer.Job
+                    + ", Aylık Geliri : " + customer.MonthlyIncome);
+            }
+        }
+
+        public  void DeleteCustomer(List<Customer> customers, int id)
+        {
+            for (int i = 0; i < customers.Count; i++)
+            {
+                if (customers[i].Id == id)
+                {
+                    customers.Remove(customers[i]);
+                }
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Customer> customers = new List<Customer>();
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            customers.Add(musteriManager.AddCustomer(1, "MusteriAdi_1", "MusteriSoyadi_1", "Meslek_1", 3000));
+            customers.Add(musteriManager.AddCustomer(2, "MusteriAdi_2", "MusteriSoyadi_2", "Meslek_2", 3500));
+            customers.Add(musteriManager.AddCustomer(3, "MusteriAdi_3", "MusteriSoyadi_3", "Meslek_3", 4000));
+            customers.Add(musteriManager.AddCustomer(4, "MusteriAdi_4", "MusteriSoyadi_4", "Meslek_4", 4500));
+            customers.Add(musteriManager.AddCustomer(5, "MusteriAdi_5", "MusteriSoyadi_5", "Meslek_5", 5000));
+
+            musteriManager.ListCustomers(customers);
+
+            Console.WriteLine("***********************************************************************************");
+
+            musteriManager.DeleteCustomer(customers, 3);
+
+            musteriManager.ListCustomers(customers);
+        }
+    }    
+}
